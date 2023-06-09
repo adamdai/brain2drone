7592ca0d89e1bc29901ae794b2fb9c4261a16373
Adam Dai <adamdai97@gmail.com>
2023-06-08 19:05:33 -0700

    working on teleop_controller

--- src/teleop_controller.py
+++ src/teleop_controller.py
@@ -1,124 +1,237 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import threading
 
 import rospy
-from typing import Tuple
-import numpy as np
-from geometry_msgs.msg import PointStamped, QuaternionStamped, PoseStamped
-import os 
-import redis
-import copy
+import pygame
 
-# Parameters
-X_BOUNDS = [-3.0, 3.0]
-Y_BOUNDS = [-2.0, 2.0]
-Z_BOUNDS = [0.0, 1.5]
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
 
-RATE = 10    # hz 
-DT = 1/RATE  # seconds
+import sys
+from select import select
 
+if sys.platform == 'win32':
+    import msvcrt
+else:
+    import termios
+    import tty
+
+
+TwistMsg = Twist
+
+msg = """
+Reading from the keyboard  and Publishing to Twist!
+---------------------------
+Moving around:
+   u    i    o
+   j    k    l
+   m    ,    .
+
+For Holonomic mode (strafing), hold down the shift key:
+---------------------------
+   U    I    O
+   J    K    L
+   M    <    >
+
+t : up (+z)
+b : down (-z)
+
+anything else : stop
+
+q/z : increase/decrease max speeds by 10%
+w/x : increase/decrease only linear speed by 10%
+e/c : increase/decrease only angular speed by 10%
+
+CTRL-C to quit
 """
-Takeoff at origin (0, 0, 1)
 
-"""
-
-
-class TeleopController:
-    def __init__(self):
-        rospy.init_node('teleop_controller_node', disable_signals=True)
-        self.rate = rospy.Rate(RATE)  # GCS rate is 20 Hz
-        self.dt = DT  # seconds
-        self.update_dt = 1.0  # seconds
-
-        print("starting Redis connection")
-        self.rc = redis.Redis(host='localhost', port=6379, db=0)
-        info = self.rc.xinfo_stream('drone:decode:stream')
-        self.newMsgId = info['first-entry'][0].decode('utf-8')
-
-        self.curr_pos = np.zeros(3)
-        self.curr_att = np.zeros(4)
-
-        self.ref_pos = np.array([0.0, 0.0, 1.0])
-
-        # Publishers
-        self.pos_pub = rospy.Publisher("gcs/setpoint/position", PointStamped, queue_size=1)
-        self.att_pub = rospy.Publisher("gcs/setpoint/attitude", QuaternionStamped, queue_size=1)
-
-        # Subscribers
-        mocap_sub = rospy.Subscriber("drone2/mavros/local_position/pose", PoseStamped, self.mocap_cb)
-
-    
-    def mocap_cb(self, data):
-        # Unpack Data
-        self.curr_pos[0] = data.pose.position.x
-        self.curr_pos[1] = data.pose.position.y
-        self.curr_pos[2] = data.pose.position.z
-
-        self.curr_att[0] = data.pose.orientation.w
-        self.curr_att[1] = data.pose.orientation.x
-        self.curr_att[2] = data.pose.orientation.y
-        self.curr_att[3] = data.pose.orientation.z
+move_bindings = {
+        'w':(1,0,0,0),
+        'a':(0,-1,0,0),
+        's':(-1,0,0,0),
+        'd':(0,1,0,0),
+    }
 
 
-    def update_ref_pos(self, event=None):
-        self.ref_pos = copy.deepcopy(self.curr_pos)
+class PublishThread(threading.Thread):
+    def __init__(self, rate):
+        super(PublishThread, self).__init__()
+        self.publisher = rospy.Publisher('cmd_vel', TwistMsg, queue_size = 1)
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.th = 0.0
+        self.speed = 0.0
+        self.turn = 0.0
+        self.condition = threading.Condition()
+        self.done = False
+
+        # Set timeout to None if rate is 0 (causes new_message to wait forever
+        # for new data to publish)
+        if rate != 0.0:
+            self.timeout = 1.0 / rate
+        else:
+            self.timeout = None
+
+        self.start()
+
+    def wait_for_subscribers(self):
+        i = 0
+        while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
+            if i == 4:
+                print("Waiting for subscriber to connect to {}".format(self.publisher.name))
+            rospy.sleep(0.5)
+            i += 1
+            i = i % 5
+        if rospy.is_shutdown():
+            raise Exception("Got shutdown request before subscribers connected")
+
+    def update(self, x, y, z, th, speed, turn):
+        self.condition.acquire()
+        self.x = x
+        self.y = y
+        self.z = z
+        self.th = th
+        self.speed = speed
+        self.turn = turn
+        # Notify publish thread that we have a new message.
+        self.condition.notify()
+        self.condition.release()
+
+    def stop(self):
+        self.done = True
+        self.update(0, 0, 0, 0, 0, 0)
+        self.join()
+
+    def run(self):
+        twist_msg = TwistMsg()
+
+        if stamped:
+            twist = twist_msg.twist
+            twist_msg.header.stamp = rospy.Time.now()
+            twist_msg.header.frame_id = twist_frame
+        else:
+            twist = twist_msg
+        while not self.done:
+            if stamped:
+                twist_msg.header.stamp = rospy.Time.now()
+            self.condition.acquire()
+            # Wait for a new message or timeout.
+            self.condition.wait(self.timeout)
+
+            # Copy state into twist message.
+            twist.linear.x = self.x * self.speed
+            twist.linear.y = self.y * self.speed
+            twist.linear.z = self.z * self.speed
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = self.th * self.turn
+
+            self.condition.release()
+
+            # Publish.
+            self.publisher.publish(twist_msg)
+
+        # Publish stop message when thread exits.
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
+        self.publisher.publish(twist_msg)
 
 
-    def run(self, event=None):
-        # Read off data from decoder outputs
-        stream = self.rc.xread({'drone:decode:stream': self.newMsgId}, count=1, block=1000)
-        self.newMsgId = stream[0][1][0][0].decode('utf-8')
-        vx = stream[0][1][0][1][b'vel_x:float32']
-        vy = stream[0][1][0][1][b'vel_y:float32']
-        vz = stream[0][1][0][1][b'vel_z:float32']
-        vr = stream[0][1][0][1][b'vel_r:float32']
-        
-        vx = np.frombuffer(vx, dtype=np.float32)
-        vy = np.frombuffer(vy, dtype=np.float32)
-        vz = np.frombuffer(vz, dtype=np.float32)
-        vr = np.frombuffer(vr, dtype=np.float32)
-        print(f'{vx} {vy} {vz} {vr}')
+def getKey(settings, timeout):
+    if sys.platform == 'win32':
+        # getwch() returns a string on Windows
+        key = msvcrt.getwch()
+    else:
+        tty.setraw(sys.stdin.fileno())
+        # sys.stdin.read() returns a string on Linux
+        rlist, _, _ = select([sys.stdin], [], [], timeout)
+        if rlist:
+            key = sys.stdin.read(1)
+        else:
+            key = ''
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
-        t_now = rospy.Time.now()
+def saveTerminalSettings():
+    if sys.platform == 'win32':
+        return None
+    return termios.tcgetattr(sys.stdin)
 
-        # Variables to publish
-        pos_msg = PointStamped()
-        att_msg = QuaternionStamped()
+def restoreTerminalSettings(old_settings):
+    if sys.platform == 'win32':
+        return
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
-        # Position
-        pos_msg.header.stamp = t_now
-        pos_msg.header.frame_id = "map"
+def vels(speed, turn):
+    return "currently:\tspeed %s\tturn %s " % (speed,turn)
 
-        self.ref_pos[0] += vx
-        self.ref_pos[1] += vy
-        self.ref_pos[2] += vz
-        pos_msg.point.x = self.ref_pos[0] 
-        pos_msg.point.y = self.ref_pos[1] 
-        pos_msg.point.z = self.ref_pos[2]
+if __name__=="__main__":
+    settings = saveTerminalSettings()
 
-        # Bound commanded position to be within the arena
-        pos_msg.point.z = np.clip(pos_msg.point.z, Z_BOUNDS[0], Z_BOUNDS[1])
-        pos_msg.point.y = np.clip(pos_msg.point.y, Y_BOUNDS[0], Y_BOUNDS[1])
-        pos_msg.point.x = np.clip(pos_msg.point.x, X_BOUNDS[0], X_BOUNDS[1])
+    rospy.init_node('teleop_controller')
+    pygame.init()
 
-        # Attitude
-        att_msg.header.stamp = t_now
-        att_msg.header.frame_id = "map"
-        
-        att_msg.quaternion.w = 1.0
-        att_msg.quaternion.x = 0
-        att_msg.quaternion.y = 0
-        att_msg.quaternion.z = 0
+    speed = rospy.get_param("~speed", 0.5)
+    turn = rospy.get_param("~turn", 1.0)
+    speed_limit = rospy.get_param("~speed_limit", 1000)
+    turn_limit = rospy.get_param("~turn_limit", 1000)
+    repeat = rospy.get_param("~repeat_rate", 0.0)
+    key_timeout = rospy.get_param("~key_timeout", 0.5)
+    stamped = rospy.get_param("~stamped", False)
+    twist_frame = rospy.get_param("~frame_id", '')
+    if stamped:
+        TwistMsg = TwistStamped
 
-        # Publish
-        self.pos_pub.publish(pos_msg)
-        self.att_pub.publish(att_msg)
+    pub_thread = PublishThread(repeat)
 
+    x = 0
+    y = 0
+    z = 0
+    th = 0
+    status = 0
 
-if __name__ == '__main__':
-    
-    dc = DecodeController()
+    try:
+        #pub_thread.wait_for_subscribers()
+        pub_thread.update(x, y, z, th, speed, turn)
 
-    rospy.Timer(rospy.Duration(dc.dt), dc.run)
-    rospy.Timer(rospy.Duration(dc.update_dt), dc.update_ref_pos)
-    rospy.spin()
+        print(msg)
+        print(vels(speed,turn))
+        while(1):
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w] and keys[pygame.K_a]:
+                print("Check")
+            # key = getKey(settings, key_timeout)
+            # if key in move_bindings.keys():
+            #     x = move_bindings[key][0]
+            #     y = move_bindings[key][1]
+            #     z = move_bindings[key][2]
+            #     th = move_bindings[key][3]
+            # else:
+            #     # Skip updating cmd_vel if key timeout and robot already
+            #     # stopped.
+            #     if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
+            #         continue
+            #     x = 0
+            #     y = 0
+            #     z = 0
+            #     th = 0
+            #     if (key == '\x03'):
+            #         break
+            
+            # print(f"{x}, {y}, {z}, {th}")
+            # pub_thread.update(x, y, z, th, speed, turn)
 
+    except Exception as e:
+        print(e)
+
+    finally:
+        pub_thread.stop()
+        restoreTerminalSettings(settings)
